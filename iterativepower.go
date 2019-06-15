68ba0fb76b2145e13602b9f9fae7910b5aa8a2f5
nuhusouleymane225 <souleymaneeladjnuhu@gmail.com>
2019-06-15 20:07:22 +0200

    nombre

---
+++ iterativepower.go
@@ -0,0 +1,18 @@
+
+package piscine
+
+import (
+	"math"
+)
+
+func IterativePower(nb int, power int) int {
+
+	if power < 0 {
+		return 0
+	}
+	x, y := float64(nb), float64(power)
+	res := math.Pow(x, y)
+
+	return int(res)
+
+}
